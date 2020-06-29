d6ab5738692930f2b7692cfaa548638447cebb37
chengfeng <klaus.cheng@qq.com>
2020-06-29 11:11:38 -0400

    change log formatter

--- tfbp/logger.py
+++ tfbp/logger.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger("tensorflow")
 LOGGER.setLevel(logging.DEBUG)
 
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s',
                               datefmt='%m-%d %H:%M')
 
 
